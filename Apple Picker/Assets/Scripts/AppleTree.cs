37e807440f1b3c8f4dff05a16a6e5ba6527ff688
toppinsb96 <31316272+toppinsb96@users.noreply.github.com>
2017-09-21 10:45:24 -0500

    Apple Picked

---
+++ Apple Picker/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+	public GameObject Apple_Prefab;
+	public float speed = 1f;
+	public float leftandRightEdge = 10f;
+	public float chanceToChangeDirections = 0.1f;
+	public float secondsBetweenAppleDrops = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+		if (pos.x < -leftandRightEdge) {
+			speed = Mathf.Abs(speed);
+		} else if (pos.x > leftandRightEdge) {
+			speed = -Mathf.Abs(speed);
+		} else if(Random.value < chanceToChangeDirections) {
+			speed *= -1;
+		}
+	}
+}
